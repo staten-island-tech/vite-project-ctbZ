e23fd2e8bf5918d2f9061d22038335bbee360f38
ctbZ <113132299+ctbZ@users.noreply.github.com>
2022-12-23 13:16:05 -0500

    asd

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -20,13 +20,30 @@ DOM.linear.addEventListener("click", function(){
 })
 
 function linearpage(){
-  switches.filter((menu) => menu.Type === "Linear").forEach((menu) => DOM.switchpage
+  switches.filter((s) => s.Type === "Linear").forEach((ls) => DOM.switchpage
   .insertAdjacentHTML(
     "beforeend",
     `<div class="cards">
-    <img class="IMG" src="${switches.IMG}"><img>
-    <h2 class="CardName">${switches.Name}</h2>
-    <h3 class="CardPrice">$${switches.Price}</h3>
+    <img class="IMG" src="${ls.IMG}"><img>
+    <h2 class="CardName">${ls.Name}</h2>
+    <h3 class="CardPrice">$${ls.Price}</h3>
+    </div>
+    `
+))
+}
+DOM.tactile.addEventListener("click", function(){
+  DOM.switchpage.innerHTML = ""
+  tactilepage();
+})
+
+function tactilepage(){
+  switches.filter((s) => s.Type === "Tactile").forEach((ts) => DOM.switchpage
+  .insertAdjacentHTML(
+    "beforeend",
+    `<div class="cards">
+    <img class="IMG" src="${ts.IMG}"><img>
+    <h2 class="CardName">${ts.Name}</h2>
+    <h3 class="CardPrice">$${ts.Price}</h3>
     </div>
     `
 ))
