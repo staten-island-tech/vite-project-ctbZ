efc57ad7c575ba6316c786beb7d11a25b0f8c95e
ctbZ <113132299+ctbZ@users.noreply.github.com>
2023-01-01 23:00:44 -0500

    kj

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -48,3 +48,21 @@ function tactilepage(){
     `
 ))
 }
+
+DOM.clicky.addEventListener("click", function(){
+  DOM.switchpage.innerHTML = ""
+  clickypage();
+})
+
+function clickypage(){
+  switches.filter((s) => s.Type === "Clicky").forEach((cs) => DOM.switchpage
+  .insertAdjacentHTML(
+    "beforeend",
+    `<div class="cards">
+    <img class="IMG" src="${cs.IMG}"><img>
+    <h2 class="CardName">${cs.Name}</h2>
+    <h3 class="CardPrice">$${cs.Price}</h3>
+    </div>
+    `
+))
+}
